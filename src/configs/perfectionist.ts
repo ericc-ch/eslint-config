5bf32f95639fcbeba17758528249d585138c2589
Erick Christian <erickchristian48@gmail.com>
2024-11-28 08:35:08 +0700

    feat: enable only small subset of perfectionist rules

--- src/configs/perfectionist.ts
+++ src/configs/perfectionist.ts
@@ -3,6 +3,15 @@ import typescriptPlugin from "typescript-eslint";
 
 export const perfectionist = (): ReturnType<typeof typescriptPlugin.config> => {
   return typescriptPlugin.config({
-    extends: [perfectionistPlugin.configs["recommended-natural"]],
+    plugins: {
+      perfectionist: perfectionistPlugin,
+    },
+    rules: {
+      "perfectionist/sort-array-includes": "error",
+      "perfectionist/sort-exports": "error",
+      "perfectionist/sort-imports": "error",
+      "perfectionist/sort-modules": "error",
+      "perfectionist/sort-named-exports": "error",
+    },
   });
 };
