d8d9e7ecf77c42571c56f102d9802c0c53dbe621
Erick Christian <erickchristian48@gmail.com>
2025-02-07 11:07:59 +0700

    update build tools

--- src/configs/react-hooks.ts
+++ src/configs/react-hooks.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
+// @ts-expect-error eslint-plugin-react-hooks don't have typing
 import reactHooksPlugin from "eslint-plugin-react-hooks"
 import typescriptPlugin from "typescript-eslint"
 
