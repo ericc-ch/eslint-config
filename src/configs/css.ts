82ff19ae871950e78b38df17f8ee8bd0edf42a9c
Erick Christian <erickchristian48@gmail.com>
2025-08-02 02:21:54 +0700

    chore: run lint

--- src/configs/css.ts
+++ src/configs/css.ts
@@ -5,8 +5,6 @@ export const cssConfig = (): ReturnType<typeof typescriptPlugin.config> => {
   return typescriptPlugin.config({
     files: ["**/*.css"],
     plugins: {
-      // @eslint/css doesn't have type definitions
-
       css,
     },
     language: "css/css",
